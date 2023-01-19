f186125be9d9e947a99bfb81e4db81729f2953ec
Ratanmondol <ratantvc@gmail.com>
2023-01-19 07:45:01 +0600

    first commit

---
+++ case.js
@@ -0,0 +1,30 @@
+// const firstName = 'RataN';
+// const secendName = 'Mondol';
+// console.log(firstName.toLowerCase());
+// if (firstName.toLowerCase() === secendName.toLowerCase()) {
+//     console.log('Valid');
+// }
+// else {
+//     console.log('invalid');
+// }
+
+
+
+const lyrics = ' amr sunar bangla ami tumai balobasi tumar aKash tumar batas';
+
+
+const lyricsLowerCase = lyrics.toLowerCase();
+// const exist = lyrics.includes('akash');
+// console.log(exist);
+
+
+if (lyrics.indexOf('ami') !== -1) {
+    console.log('Exist inside');
+}
+else {
+    console.log('Can not find');
+}
+
+
+
+
